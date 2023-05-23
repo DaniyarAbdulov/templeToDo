12a921ac8c59531b0ba280546202aca1dde433b8
daniyarabdulov <daniyar0303@gmail.com>
2023-05-24 01:17:47 +0600

    AsynsEdit and AsyncRemove added

--- src/redux/slices/asyncSlice.ts
+++ src/redux/slices/asyncSlice.ts
@@ -22,13 +22,18 @@ const asyncSlice = createSlice({
 		builder.addCase(fetchTodos.fulfilled, (state, {payload}) => ({...state, loading: false, todos: payload}))
 		builder.addCase(fetchTodos.rejected, state => ({...state, loading: false, todos: []}))
 		builder.addCase(removeTodoAsync.pending, state => ({...state, loading: true}))
-		builder.addCase(removeTodoAsync.fulfilled, (state, {payload, meta}) => ({...state, loading: false}))
+		builder.addCase(removeTodoAsync.fulfilled, (state, {payload, meta}) => ({...state, todos:state.todos.filter(todo=>todo.id!== meta.arg), loading: false}))
 		builder.addCase(removeTodoAsync.rejected, state => ({...state, loading: false}))
 		builder.addCase(editTodoAsync.pending, state => ({...state, loading: true}))
-		builder.addCase(editTodoAsync.fulfilled, (state, {payload}) => ({
-			...state,
-			loading: false
-		}))
+		builder.addCase(editTodoAsync.fulfilled, (state, {payload}) => {
+			const {id,title} = payload
+			const index = state.todos.findIndex((todo) => todo.id === id)
+			if (index!== -1){
+				state.todos[index].title = title
+			}
+			state.loading=false
+			return state
+		})
 		builder.addCase(editTodoAsync.rejected, state => ({...state, loading: false}))
 	}
 })
--- src/redux/thuks/asyncThunk.ts
+++ src/redux/thuks/asyncThunk.ts
@@ -12,10 +12,20 @@ export const fetchTodos = createAsyncThunk<IToDo[]>('todos/fetch', async () => {
 })
 
 export const removeTodoAsync = createAsyncThunk('todos/remove', async (id:number) => {
-	return await api.delete (`/todos/${id}`).then(res => res.data).catch(e => console.error(e.response ?? e))
+	return await api.delete (`/todos/${id}`).then(res => {
+		if (res.status !== 200){
+			throw Error("Request failed")
+		}
+		return res.data
+	}).catch(e => console.error(e.response ?? e))
 })
 
-export const editTodoAsync = createAsyncThunk('todos/edit', async (data:{id: number; title: string}) => {
+export const editTodoAsync = createAsyncThunk<IToDo, {id: number; title: string}>('todos/edit', async (data:{id: number; title: string}) => {
 	const {id, title} = data
-	return await api.put(`/todos/${id}`, {'title': title}).then(res => res.data).catch(e => console.error(e.response ?? e))
+	return await api.put(`/todos/${id}`, {'title': title}).then(res => {
+		if (res.status !== 200){
+			throw Error("Request failed")
+		}
+		return res.data
+	}).catch(e => console.error(e.response ?? e))
 })
